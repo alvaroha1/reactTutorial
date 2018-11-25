60d592dce4120d5c5ac9e1fbdcd26a1013c98770
alvaroha1 <alvarohernandezassens@gmail.com>
2018-11-25 19:36:59 +0100

    fix: basic feats

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 
 import classes from './Toolbar.css';
+import NavigationItems from '../NavigationItems/NavigationItems'
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo />
+        <DrawerToggle clicked={props.drawerToggleClicked}/>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
         <nav>
-            ...
+            <NavigationItems />
         </nav>
     </header>
 );
